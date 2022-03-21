09de3edaacc7ead7350a31f5b3783fdefb0c6e20
Aditya Maheshwari <nag60734@adobe.com>
2022-03-21 22:27:14 +0530

    CSS Changes only

--- public/js/lobby-page.js
+++ public/js/lobby-page.js
@@ -1,5 +1,7 @@
 let lobbyId = $('.hidden-lobby-id').text();
 let userName = $('.hidden-username').text();
+let totalTeams = 2;
+var c=0;
 const socket = io();
 
 socket.on('connect',()=>{
@@ -13,10 +15,17 @@ socket.on('welcome',()=>{
     socket.emit('join-room');
 });
 $('.header-right-text').on('click',()=>{
+    c++;
     console.log("clicked");
-    $('.lobby-notification').append('<span>Someone has entered</span>')
+    const temp =['blue-alert-box','red-alert-box','green-alert-box'];
+     $('.lobby-notification').prepend(`<div><strong>Someone has entered ${c}</strong><div>`)
+     const tempNotif = $('.lobby-notification>div').first();
+     $(tempNotif).addClass(temp[Math.floor(Math.random()*3)]);
+    setTimeout(function(){
+        $(tempNotif).remove();
+      }, 3000);
 })
-//$('.cmp-lobby-chat').scrollTop($('.cmp-lobby-chat').height())
+
 socket.on('joined',(currentLobby)=>{
     console.log("you have joined this lobby");
     console.log(currentLobby);
@@ -36,13 +45,66 @@ socket.on('player-left',(currentLobby)=>{
     console.log(currentLobby);
     updateJoinedList(currentLobby.players);
 });
-$('.start-game').on('click',function(){
-    socket.emit('start-game','data');
+
+$('.send-chat').on('click',()=>{
+    const chat = $('.cmp-input-message').val();
+    if(chat){
+        addChat(chat);
+    }
+})
+$('.cmp-input-message').on('keypress',function(e) {
+    if(e.which == 13) {
+        const chat = $('.cmp-input-message').val();
+        if(chat){
+            addChat(chat);
+        }
+    }
 });
 
+
+$('.cmp-joined-member input[type="checkbox"]').on('change', function(evt) {
+    const selectedPlayers = $('.cmp-joined-member input[type="checkbox"]:checked');
+    const startGame = $('.start-game');
+   if(selectedPlayers.length < totalTeams) {
+    $('.cmp-joined-member input[type="checkbox"]').removeClass('not-allowed');
+    startGame.addClass('inactive-button');
+   }
+    if(selectedPlayers.length == totalTeams){
+        $('.cmp-joined-member input[type="checkbox"]:not(:checked)').addClass('not-allowed');
+    startGame.removeClass('inactive-button');
+
+    }
+   if(selectedPlayers.length > totalTeams) {
+       this.checked = false;
+
+   }
+});
+$('.start-game').on('click',function(){
+    const selectedPlayers = $('.cmp-joined-member input[type="checkbox"]:checked');
+    if(selectedPlayers.length<totalTeams){
+        
+    }
+    selectedPlayers.each(function(index,ele){
+        console.log($(ele).attr('name'));
+    })
+});
 function updateJoinedList(playerList){
     $('.member-list').html('')
     for(let i=0;i<playerList.length;i++){
-        $('.member-list').append(`<li>${playerList[i]}</li>`);
+        $('.member-list').append(`<li><div>${playerList[i]}</div><input type="checkbox"></li>`);
     }
+}
+
+function getRandomColorBox(){
+    let colorBox =['blue-alert-box','red-alert-box','green-alert-box','yellow-alert-box'];
+    let randomIndex = Math.floor(Math.random()*colorBox.length);
+    return colorBox[randomIndex];
+}
+
+function addChat(chat){
+    let randomColorBox = getRandomColorBox();
+    
+    $('.cmp-lobby-chat').append(`<fieldset><legend><i class='fa fa-user' ></i>Aditya</legend><div class=${randomColorBox}>${chat}</div></fieldset>`);
+    $('.cmp-input-message').val('');
+    $('.cmp-lobby-chat').scrollTop($('.cmp-lobby-chat').prop('scrollHeight'));
 }
